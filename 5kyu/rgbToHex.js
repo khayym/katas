7211c4b5a21140bab0c5af49c7a76d910eea6b9c
khayym <xeyyam.kerimov@gmail.com>
2022-04-26 04:17:56 +0400

    rgb convert

---
+++ 5kyu/rgbToHex.js
@@ -0,0 +1,8 @@
+function rgb(r, g, b) {
+    function toHex(a) {
+        if (a <= 0) return '00';
+        else if (a >= 255) return 'FF';
+        else return a.toString(16).toUpperCase();
+    }
+    return toHex(r) + toHex(g) + toHex(b);
+}
